8ff9cbd5e17fc0e403b3a11a226f09aec5f734bb
Ben Sheldon <bensheldon@gmail.com>
2015-03-13 18:21:02 -0700

    Update Puma configuration

---
+++ config/puma.rb
@@ -0,0 +1,18 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = threads_count
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
